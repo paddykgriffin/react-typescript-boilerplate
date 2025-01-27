70eef0d97232b1a15766639787e32583bb77d8c6
Paddy Griffin <paddygriffin@gmail.com>
2025-01-27 15:34:28 +0000

    added packages for the store

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,15 @@ import 'normalize.css';
 import './styles/global.scss';
 
 import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import persistStore from 'redux-persist/es/persistStore';
+
 import Bootstrap from './Bootstrap';
+
+let persistor = persistStore(store);
 
 if (process.env.NODE_ENV === 'production') {
   disableReactDevTools();
@@ -16,7 +24,11 @@ if (rootElement) {
   const root = ReactDOMClient.createRoot(rootElement);
   root.render(
     <React.StrictMode>
-      <Bootstrap />
+     <Provider store={store}>
+        <PersistGate loading={<div>loading</div>} persistor={persistor}>
+          <Bootstrap />
+        </PersistGate>
+      </Provider>
     </React.StrictMode>,
   );
 } else {
